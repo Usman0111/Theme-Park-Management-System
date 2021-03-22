a5a3045b4bb3c78b64722e697a2559aacd9be65b
Muhammad Usman <mohammadusmanxero@gmail.com>
2021-03-21 23:51:37 -0500

    noice little edit

--- Backend/index.js
+++ Backend/index.js
@@ -11,46 +11,13 @@ app.use("/auth", require("./routes/jwtAuth"));
 
 app.use("/attraction", require("./routes/attraction"));
 
-// app.get("/person", async (req, res) => {
-//   try {
-//     const people = await pool.query("SELECT * FROM person");
-//     res.json(people.rows);
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// });
-
-// app.post("/person", async (req, res) => {
-//   try {
-//     const person = req.body;
-
-//     const newPerson = await pool.query(
-//       "INSERT INTO person (name,dob) VALUES ($1,$2) RETURNING *",
-//       [person.name, person.dob]
-//     );
-
-//     res.json(newPerson.rows[0]);
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// });
-
-// app.put("/person/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const person = req.body;
-
-//     const updatePerson = await pool.query(
-//       "UPDATE person SET name=$1 WHERE id=$2",
-//       [person.name, id]
-//     );
-//     console.log(updatePerson);
-
-//     res.json(updatePerson.rows);
-//   } catch (err) {
-//     console.log(err.message);
-//   }
-// });
+app.get("", async (req, res) => {
+  try {
+    res.send("server running boi");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server listening on part 5000");
