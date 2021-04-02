fdb1229c57e13c0daa62a661f66f5be91215d186
Muhammad Usman <mohammadusmanxero@gmail.com>
2021-04-01 23:11:55 -0500

    added base url

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import DataContextProvider from "./contexts/dataContext";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://35.217.91.119:8000/api";
+axios.defaults.baseURL = "http://100.26.17.215:5000/";
 
 ReactDOM.render(
   <React.StrictMode>
