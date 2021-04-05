7bf0dc7caa98b2047bfed8e36fb033fa66540a24
Muhammad Usman <mohammadusmanxero@gmail.com>
2021-04-05 00:31:30 -0500

    added react build

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
-const pool = require("./db");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, "../Frontend/build")));
 
 app.use("/auth", require("./routes/jwtAuth"));
-
 app.use("/attraction", require("./routes/attraction"));
 app.use("/ride", require("./routes/ride"));
 app.use("/customer", require("./routes/customer"));
 app.use("/manager", require("./routes/manager"));
 
-app.get("", async (req, res) => {
-  try {
-    res.send("server running boi");
-  } catch (err) {
-    console.log(err.message);
-  }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../Frontend/build", "index.html"));
 });
 
 app.listen(5000, () => {
