64bc964002ceda53e41e671530dd6146cbf379d6
Muhammad Usman <mohammadusmanxero@gmail.com>
2021-04-10 19:08:55 -0500

    add pass updates

--- Frontend/src/pages/CustomerPages/CustomerPass.js
+++ Frontend/src/pages/CustomerPages/CustomerPass.js
@@ -1,39 +1,54 @@
-import React  from "react";
+import React, { useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 import { useState } from "react";
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Box from '@material-ui/core/Box';
-import { useHistory } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Box from "@material-ui/core/Box";
 import Snackbar from "@material-ui/core/Snackbar";
-import Alert from '@material-ui/lab/Alert';
+import Alert from "@material-ui/lab/Alert";
+import Grid from "@material-ui/core/Grid";
+import grey from "@material-ui/core/colors/grey";
 
 const useStyles = makeStyles((theme) => ({
   card: {
-    display: 'flex',
+    display: "flex",
     maxWidth: 560,
-    backgroundColor: '#1c2e4a',
+    backgroundColor: "#1c2e4a",
   },
   pos: {
     marginBottom: 12,
   },
-  id:{
+  id: {
     fontSize: 11,
   },
   content: {
-    flex: '1 0 auto',
+    flex: "1 0 auto",
   },
   cover: {
     width: 200,
   },
 }));
 
+const createPass = (pass) => {
+  const indexT = pass.time_purchased.indexOf("T");
+  const indexDot = pass.time_purchased.indexOf(".");
+  const indexZ = pass.time_purchased.indexOf("Z");
+  return {
+    date: pass.time_purchased.slice(0, indexT),
+    time: pass.time_purchased.slice(indexT + 1, indexDot),
+    ticketId:
+      String(pass.customer_id) +
+      String(pass.entrypass_id) +
+      String(pass.time_purchased.slice(indexDot + 1, indexZ)),
+    expired: pass.expired,
+  };
+};
 
-function CustomerPass(props){
+function CustomerPass(props) {
   const classes = useStyles();
 
   const [open, setOpen] = useState(false);
@@ -47,158 +62,101 @@ function CustomerPass(props){
     setOpen(false);
   };
 
-  let history = useHistory();
+  const [tickets, setTickets] = useState([]);
 
-  var i = 0;
-  var info = []
-  var entryPassId = []
-  var timePurchased = []
-  var datePurchased = []
-  var ticketStatus = []
-  var arr =[]
-
- 
-  const [tickets, setTickets] = useState([])
-  
-
-  const buy_ticket= (e) => {
+  const buy_ticket = (e) => {
     e.preventDefault();
-    console.log(e);
     axios
-    .post("http://100.26.17.215:5000/customer/purchase-pass", {user_id: localStorage.getItem("user_id")})
-    .then((res) => {
-      alert("You have successfully purchased a ticket! To view current and previous tickets please click on 'View All Tickets'")
-      console.log(res);
-    })
-    .catch((err)=> {
-      setErr(err.res.data);
-      console.log(err.res.data);
-      setOpen(true);
-    })
+      .post("http://100.26.17.215:5000/customer/purchase-pass", {
+        user_id: localStorage.getItem("user_id"),
+      })
+      .then((res) => {
+        console.log(createPass(res.data));
+        console.log(
+          "You have successfully purchased a ticket! To view current and previous tickets please click on 'View All Tickets'"
+        );
+      })
+      .catch((err) => {
+        setErr(err.res.data);
+        console.log(err.res.data);
+        setOpen(true);
+      });
   };
 
- 
-  
-  
-  const view_ticket = (e) => {
-    e.preventDefault();
-    console.log(e);
+  useEffect(() => {
     axios
-    .post("http://100.26.17.215:5000/customer/get-all-pass", {user_id: localStorage.getItem("user_id")})
-    .then((res) => {
-      console.log(res.data)
-      setTickets(res.data)
-      /*
-      while(i < res.data.length){
-        setEntryPassId(res.data[i].entrypass_id);
-        setTimePurchased(res.data[i].time_purchased.substring(14, 23));
-        setDatePurchased(res.data[i].time_purchased.substring(0, 10))
-        if(res.data[i].expired === false){
-          setStatus("Valid")
-        }
-        else{
-          setStatus("Expired")
-        } 
-         
-        i++
-      }*/
-      
-      /*
-      while(i < res.data.length){
-        info[i] = res.data[i]
-        entryPassId[i] = res.data[i].entrypass_id
-        timePurchased[i] = res.data[i].time_purchased.substring(14, 23)
-        datePurchased[i] = res.data[i].time_purchased.substring(0, 10)
-        if(res.data[i].expired === false){
-          ticketStatus[i] = ("Valid")
-        }
-        else{
-          ticketStatus[i] = ("Expired")
-        } 
-        
-        i++
-      }*/
-      //return info;
-    })   
-    .catch((err)=> {
-      setErr(err.res.data);
-      console.log(err.res.data);
-      setOpen(true);
-    })
-  }; 
-//info = view_ticket;
-  
-    
-  return (
+      .post("http://100.26.17.215:5000/customer/get-all-pass", {
+        user_id: localStorage.getItem("user_id"),
+      })
+      .then((res) => {
+        console.log(res.data);
+        const passes = res.data.map((pass) => createPass(pass));
+        console.log(passes);
+        setTickets(passes);
+      })
+      .catch((err) => {
+        console.log(err.res);
+      });
+  }, []);
 
-  <div>
-    <form onSubmit={(e) => buy_ticket(e)}>
-      <div>
-        <Button
-        variant="contained"
-        color="primary"
-        type="submit">
-          Purchase Ticket
-        </Button>
-      </div>
-    </form>
-    <br>
-    </br>
-    <form onSubmit={(e) => view_ticket(e)}>
-      <div>
-        <Button
-        variant="contained"
-        color="primary"
-        type="submit">
-          View All Tickets
-        </Button>
-      </div>
-      </form>
-    <br>
-    </br>
+  return (
     <div>
-      {tickets.map(ticket => 
+      <form onSubmit={(e) => buy_ticket(e)}>
         <div>
-          {ticket.time_purchased}
+          <Button variant="contained" color="primary" type="submit">
+            Purchase Ticket
+          </Button>
         </div>
-      )}
+      </form>
+      <br></br>
+
+      <Grid container spacing={3}>
+        {tickets.map((ticket) => (
+          <Grid item xs={6}>
+            <Card className={classes.card}>
+              <CardContent className={classes.content}>
+                <Box color="#FFFFFF">
+                  <Typography variant="h5" component="h2" align="center">
+                    Day Entry Pass
+                  </Typography>
+                  <Typography className={classes.pos}>
+                    <span style={{ color: grey[400] }}>Date Purchased:</span>{" "}
+                    {ticket.date}
+                  </Typography>
+                  <Typography className={classes.pos}>
+                    <span style={{ color: grey[400] }}>Time Purchased:</span>{" "}
+                    {ticket.time}
+                  </Typography>
+                  <Typography className={classes.pos}>
+                    <span style={{ color: grey[400] }}>Status:</span>{" "}
+                    {ticket.expired ? (
+                      <span style={{ color: "red" }}>expired</span>
+                    ) : (
+                      <span style={{ color: "green" }}>valid</span>
+                    )}
+                  </Typography>
+                  <Typography className={classes.id}>
+                    <span style={{ color: grey[400] }}>Ticket ID:</span>{" "}
+                    {ticket.ticketId}
+                  </Typography>
+                </Box>
+              </CardContent>
+              <CardMedia
+                className={classes.cover}
+                image="https://media.beam.usnews.com/de/2248f0a712a4c92fa641b0dd037bb7/media:01216eb80ba74e33b55f2ebe4d36faceTheme_Parks-Slow_Reopening_73567.jpg"
+              />
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+        <Alert severity="error" style={{ marginTop: "10px" }}>
+          {err}
+        </Alert>
+      </Snackbar>
     </div>
-    
-    <div  onSubmit={(e) => view_ticket(e)} >
-    <Card className={classes.card}>
-        <CardContent className={classes.content}>
-          <Box color="#FFFFFF">
-          <Typography variant="h5" component="h2" align="center">
-            Day Entry Pass
-          </Typography>
-          <Typography className={classes.pos}>
-            Date Purchased: {}
-          </Typography>
-          <Typography className={classes.pos}>
-            Time Purchased: {}
-          </Typography>
-          <Typography className={classes.pos} >
-            Status: {}
-          </Typography>
-          <Typography className={classes.id}>
-            Ticket ID: {}
-          </Typography>
-          </Box> 
-         </CardContent>
-         <CardMedia
-           className={classes.cover}
-           image="https://media.beam.usnews.com/de/2248f0a712a4c92fa641b0dd037bb7/media:01216eb80ba74e33b55f2ebe4d36faceTheme_Parks-Slow_Reopening_73567.jpg"
-         />
-         </Card>
-    </div>
-    <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
-      <Alert severity="error" style={{ marginTop: "10px" }}>
-        {err}
-      </Alert>
-    </Snackbar>
-  </div>
-  
   );
-};
+}
 
 export default CustomerPass;
