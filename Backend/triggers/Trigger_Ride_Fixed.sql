4828d4ee9bf4320765be92e538a5ed80f5933bf3
Muhammad Usman <mohammadusmanxero@gmail.com>
2021-04-24 20:22:02 -0500

    altered trigger

--- Backend/triggers/Trigger_Ride_Fixed.sql
+++ Backend/triggers/Trigger_Ride_Fixed.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE FUNCTION maintainer_notify_attendant()
   AS
 $$
 BEGIN
-	IF NEW.maintainer_id IS NOT NULL THEN
+	IF NEW.maintainer_id IS NOT NULL AND NEW.fullfilled = TRUE THEN
 		PERFORM pg_notify('ride_fixed_notification', '{"message":"Your assigned ride has been fixed", "attendant_id":'||NEW.attendant_id||', "ride_id":'||NEW.ride_id||', "maintainer_id": '||NEW.maintainer_id||'}');
 	END IF;
 	RETURN NEW;
